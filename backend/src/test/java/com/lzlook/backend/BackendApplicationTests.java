d49a11c42f86e80fd7c1b7b06f32e8a166019a54
litex tech 05 <1123084484@qq.com>
2019-10-18 15:06:54 +0800

    add module: com.lzlook.backend

---
+++ backend/src/test/java/com/lzlook/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzlook.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
