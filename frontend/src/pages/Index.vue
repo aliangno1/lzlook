f2ec88a4a912fa56c29c6e1cac39cdeb3c5e165a
litex tech 05 <1123084484@qq.com>
2019-10-31 18:53:08 +0800

    add lzlook-plants image

--- frontend/src/pages/Index.vue
+++ frontend/src/pages/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page class="full-height">
     <a href="https://github.com/aliangno1/lzlook">
-    <img
+      <img
         width="149"
         height="149"
         src="https://github.blog/wp-content/uploads/2008/12/forkme_left_orange_ff7600.png?resize=149%2C149"
@@ -11,16 +11,26 @@
     /></a>
     <q-form
       @submit="search(keyword)"
-      style="top:40%;position:fixed;"
+      style="top:25%; position:fixed;"
       class="full-width"
     >
-      <div class="row justify-center" id="form">
+      <div class="row q-mb-sm justify-center">
+        <div class="col-md-4 col-xs- text-center">
+          <img
+            src="../statics/logos/lzlook-plants.png"
+            alt="lzlook"
+            width="350"
+          />
+        </div>
+      </div>
+
+      <div class="row justify-center q-mb-sm" id="form">
         <q-input
           outlined
           dense
           v-model="keyword"
           placeholder="请输入完整小说名"
-          class="col-sm-6"
+          class="col-sm-8 col-md-6 col-xs-11"
           style="max-width:600px"
         ></q-input>
       </div>
@@ -28,7 +38,7 @@
       <div class="column" id="input">
         <div class="col self-center row"></div>
         <div class="col self-center q-mt-md">
-          <q-btn type="submit" label="Lz Search" color="black" no-caps></q-btn>
+          <q-btn type="submit" label="Lz Search" color="primary" no-caps></q-btn>
         </div>
       </div>
     </q-form>
