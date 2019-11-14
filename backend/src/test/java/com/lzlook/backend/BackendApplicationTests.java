0c0aed30fa8fea4eac9894f6df14e5ef9e187f16
litex tech 05 <1123084484@qq.com>
2019-11-14 13:56:08 +0800

    update springboot version to 2.1.5

--- backend/src/test/java/com/lzlook/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/lzlook/backend/BackendApplicationTests.java
@@ -1,13 +1,8 @@
 package com.lzlook.backend;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
